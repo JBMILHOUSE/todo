c40f07a110b72da45c93e49e06fc4a4875ac7abb
Christopher Alexandre <christopher.alexandre.oliver@gmail.com>
2026-03-22 21:03:57 -0300

    criando componente icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import TrashIcon from "./assets/icons/trash.svg?react";
+import Icon from "./components/icon";
+
 function App() {
   return (
-   <div>Olá mundo</div>
+   <div>
+
+    <div className="flex gap-1">
+      <Icon svg={TrashIcon} className="fill-pink-base"/>
+    </div>
+   </div>
   )
 }
 
