38183067c470561b84b18c5c2814ba868a6c5c01
Christopher Alexandre <christopher.alexandre.oliver@gmail.com>
2026-06-12 15:48:52 -0300

    criando componente de tarefa part 2

--- src/core-components/task-item.tsx
+++ src/core-components/task-item.tsx
@@ -3,16 +3,28 @@ import Card from "../components/card";
 import InputCheckbox from "../components/input-checkbox";
 import Text from "../components/text";
 
+import TrashIcon from "../assets/icons/trash.svg?react";
+import PencilIcon from "../assets/icons/pencil.svg?react";
+
+import { useState } from "react";
 
 export default function TaskItem() {
-  
+  const [isEditing, setIsEditing] = useState(false);
+
   return(
-  <Card size="md" className="flex items-center gap-3">
-      <InputCheckbox />
-      <Text>Fazer compras da semana</Text>
+  <Card size="md" className="flex items-center gap-4">
+     {isEditing ? (
+      <>
+         <InputCheckbox />
+      <Text className="flex-1"> fazendo compras da semana</Text>
       <div className="flex gap-1">
-        <ButtonIcon icon={} />
+        <ButtonIcon icon={TrashIcon} variant="tertiary" />
+        <ButtonIcon icon={PencilIcon} variant="tertiary" />
       </div>
+      </>
+     ) : (
+      <>Editamda¹¹</>
+     ) }
     </Card>
   )
 }
